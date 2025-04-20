cda202282c13a215588ad23f3cac62e184bd500a
markg <17348049+sockcrates@users.noreply.github.com>
2025-04-20 16:08:40 -0400

    test: use act pattern

--- tests/vector_test.c
+++ tests/vector_test.c
@@ -2,13 +2,14 @@
 
 #include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 void CreatingEmptyVector() {
   printf("Running CreatingEmptyVector()…\n");
   Vector* vec = CreateVector();
+
   assert(vec != NULL);
   void* data = PeekVector(vec, 0);
+
   assert(data == NULL);
   DestroyVector(vec);
 }
